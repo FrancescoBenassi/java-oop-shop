fd2e7a83cd2da5d63d3b9f7d828a108a6e41eeb3
FrancescoBenassi <francybenna77@gmail.com>
2024-11-20 10:48:02 +0100

    feat: added builder

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -7,6 +7,15 @@ public class Prodotto {
     String description;
     int price;
     int iva;
+        
+    public Prodotto(String name, String description, int price, int iva){
+        double doubleRandomNumber = Math.random() * 1000;
+        this.code = (int)doubleRandomNumber;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
 
 
 }
